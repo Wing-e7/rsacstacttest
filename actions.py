faeedb3abc80c394ffe0f2711f60f4a848dd1ca4
Wing-e7 <62994894+Wing-e7@users.noreply.github.com>
2020-06-08 15:26:04 +0530

    Add files via upload

---
+++ actions.py
@@ -0,0 +1,27 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import pandas
+#
+class Sample(Action):
+
+     def name(self) -> Text:
+         return "sample"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+         dispatcher.utter_message(text="sample action works in rasa x")
+
+         return []
